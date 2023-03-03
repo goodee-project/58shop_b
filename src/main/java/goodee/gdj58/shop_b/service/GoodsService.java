dd048c904c896557170ef3446bc1f1f68e435253
ldk77 <yd892@naver.com>
2023-03-03 16:05:33 +0900

    [이동규] 서비스 클래스 생성

--- src/main/java/goodee/gdj58/shop_b/service/GoodsService.java
+++ src/main/java/goodee/gdj58/shop_b/service/GoodsService.java
@@ -4,21 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import goodee.gdj58.shop_b.mapper.TestMapper;
-import goodee.gdj58.shop_b.vo.Test;
-
 @Service
 @Transactional
-public class TestService {
-
-	@Autowired
-	private TestMapper testMapper;
-	
-	public Test getTest(int no) {
-		
-		return testMapper.selectTest(no);
-		
-	}
-	
+public class GoodsService {
 	
 }
