506b4692286fcd145c3628c1cc2a858d8a813142
ldk77 <yd892@naver.com>
2023-03-07 12:18:43 +0900

    [이동규] 문의사항-고객 답변 맵퍼

--- src/main/java/goodee/gdj58/shop_b/mapper/AnswerCustomerCompanyMapper.java
+++ src/main/java/goodee/gdj58/shop_b/mapper/AnswerCustomerCompanyMapper.java
@@ -1,8 +1,17 @@
 package goodee.gdj58.shop_b.mapper;
 
+
 import org.apache.ibatis.annotations.Mapper;
+
+import goodee.gdj58.shop_b.vo.AnswerCustomerCompany;
 
 @Mapper
 public interface AnswerCustomerCompanyMapper {
-
+	// 수정
+	int updateAnswerCustomer(int answerCustomerCompanyNo);
+	// 삭제
+	int deleteAnswerCustomer(int answerCustomerCompanyNo);
+	// 등록
+	int insertAnswerCustomer(AnswerCustomerCompany answerCustomerCompany);
+	
 }
